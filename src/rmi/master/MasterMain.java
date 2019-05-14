33a7b741460bcf8ca5c68adb7577a7a831caa204
Michael <micoraafat96@gmail.com>
2019-05-14 17:59:34 +0200

    exporting master object

--- src/rmi/master/MasterMain.java
+++ src/rmi/master/MasterMain.java
@@ -1,5 +1,11 @@
 package rmi.master;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+
+
 /**
  * CS 432: Distributed Systems.
  * Assignment 3: Relpicated File System
@@ -8,7 +14,33 @@ package rmi.master;
  * Tuesday, 07 May 2019
  */
 public class MasterMain {
+	private static int port;
+	private static int rmiPort = 1099;
+    private static String serverAddress;
+    private static String dir;
+    private static Registry reg;
+    private static Master controller;
+    private static MasterServerClientInterface rController;
     public static void main(String[] args) {
-
+    	if (args[0] == "-ip") {
+    		serverAddress = args[1];
+    	}
+    	if (args[2] == "-port") {
+    		port = Integer.parseInt(args[3]);
+    	}
+    	if (args[4] == "-dir") {
+    		dir = args[5];
+    	}
+    	System.setProperty("java.rmi.server.hostname",serverAddress);
+    	try {
+    		LocateRegistry.createRegistry(rmiPort);
+    		rController = (MasterServerClientInterface) UnicastRemoteObject.exportObject(controller, port);
+    		reg = LocateRegistry.getRegistry(rmiPort);
+    		reg.rebind("RemoteAccessController", rController);
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    	
+    	
     }
 }
